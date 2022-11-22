d46415aae836ed7d0cc7c501f09ffb6729e7105f
Ridam Garg <ridamgarg8979@gmail.com>
2022-11-23 00:44:43 +0530

    Created Carousel

--- client/src/Components/product/allProducts.js
+++ client/src/Components/product/allProducts.js
@@ -33,6 +33,88 @@ class allProducts extends React.Component {
       <div>
         <Navbar />
         <Flash />
+        <div>
+          <div
+            id={`carousel-1`}
+            className="carousel slide"
+            data-bs-ride="carousel"
+            data-bs-interval="3000"
+            h="50"
+          >
+            <div class="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target={`#carousel-1`}
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                type="button"
+                data-bs-target={`#carousel-1`}
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              ></button>
+              <button
+                type="button"
+                data-bs-target={`#carousel-1`}
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              ></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  style={{ height: '60vh' }}
+                  src="https://s3b.cashify.in/gpro/uploads/2019/09/16135517/buyback_offer_banner.png"
+                  class="d-block w-100 carousel-offer-img"
+                  alt="..."
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  style={{ height: '60vh' }}
+                  src="https://www.rummycentral.com/static/promotion_images/diwali-dhamka-inner-promotion.f14592593a32.png"
+                  class="d-block w-100 carousel-offer-img"
+                  alt="..."
+                />
+              </div>
+              <div class="carousel-item">
+                <img
+                  style={{ height: '60vh' }}
+                  src="https://s3b.cashify.in/gpro/uploads/2019/09/16135517/buyback_offer_banner.png"
+                  class="d-block w-100 carousel-offer-img"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <button
+              class="carousel-control-prev"
+              type="button"
+              data-bs-target={`#carousel-1`}
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next"
+              type="button"
+              data-bs-target={`#carousel-1`}
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+        </div>
         <div
           className="row justify-content-around mt-3 d-flex"
           id="allProducts"
